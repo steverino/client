7fe35fb10bb1810b89815f491f20c5aa385aced2
steverino33 <steve.f@megacatstudios.com>
2024-04-24 11:24:40 -0400

    delete added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+import TodoForm from './TodoForm';
+import './styles/App.css'; // Import the custom styles
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    // Fetch data from the Express server
+    axios.get('http://localhost:5000/todos')
+      .then(response => setTodos(response.data))
+      .catch(error => console.error(error));
+  }, []);
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+  const deleteTodo = async (_id) => {
+    console.log(_id);
+    try {
+      await axios.delete('http://localhost:5000/todos/:_id')
+      .then(response => {
+        setTodos(todos.filter(todo => todo._id !== _id))
+        console.log('Todo deleted successfully:', response.data);
+      })
+      
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>MERN Stack Todo App</h1>
+      <TodoForm onAdd={addTodo} />
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>{todo.task} {todo._id}
+          
+          <button className='btn-delete' onClick={()=>deleteTodo(todo._id)}>Delete Todo</button>
+          </li>
+          
+        ))}
+      </ul>
     </div>
   );
-}
-
+};
 export default App;
